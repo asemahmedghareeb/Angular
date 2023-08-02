1d4462d14e75f8b4b1b72d7675f80137ee74bc5b
aseme <aseme@ASEM>
2023-08-02 21:25:04 +0300

    new

--- shoppig_app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shoppig_app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import {Recipe} from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes:Recipe[]=[
+    new Recipe('A Test Recipe',"ths is simply test","https://th.bing.com/th/id/OIP.JZzRmjJcqrrHpM2MXLjK7AHaIT?w=157&h=180&c=7&r=0&o=5&pid=1.7"),
+    new Recipe('A Test Recipe',"ths is simply test","https://th.bing.com/th/id/OIP.JZzRmjJcqrrHpM2MXLjK7AHaIT?w=157&h=180&c=7&r=0&o=5&pid=1.7"),
+    new Recipe('A Test Recipe',"ths is simply test","https://th.bing.com/th/id/OIP.JZzRmjJcqrrHpM2MXLjK7AHaIT?w=157&h=180&c=7&r=0&o=5&pid=1.7"),
+  ]
 }
