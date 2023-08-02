38510957603edbe183eefa5018d90c01645f003f
aseme <aseme@ASEM>
2023-08-03 00:09:35 +0300

    new

--- debugging/debugging/src/app/app.component.ts
+++ debugging/debugging/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  servers=[];
+  servers:string[]=[];
 
   onAddServer() {
     this.servers.push(`Server no ${this.servers.length+1}`);
